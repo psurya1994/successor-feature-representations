378939ffd5d3c7d94df1c91e8c607846d10314e5
unknown <surya.penmetsa@mail.mcgill.ca>
2020-03-28 01:24:51 -0400

    Added new DSR agent -- updating psi without the use of target network.

---
+++ deep_rl/agent/DSR_agent_v2.py
@@ -0,0 +1,146 @@
+#######################################################################
+# Copyright (C) 2017 Shangtong Zhang(zhangshangtong.cpp@gmail.com)    #
+# Permission given to modify the code as long as you keep this        #
+# declaration at the top                                              #
+#######################################################################
+
+"""
+DQN code modified to convert it to DSR by Surya.
+The representations are not learned in this network.
+
+"""
+
+from ..network import *
+from ..component import *
+from ..utils import *
+import time
+from .BaseAgent import *
+
+
+class DSRActor(BaseActor):
+    def __init__(self, config):
+        BaseActor.__init__(self, config)
+        self.config = config
+        self.start()
+
+    def _transition(self):
+        if self._state is None:
+            self._state = self._task.reset()
+        config = self.config
+        with config.lock:
+            _, psi, q_values = self._network(config.state_normalizer(self._state))
+        q_values = to_np(q_values).flatten()
+        if self._total_steps < config.exploration_steps \
+                or np.random.rand() < config.random_action_prob():
+            action = np.random.randint(0, len(q_values))
+        else:
+            action = np.argmax(q_values)
+        next_state, reward, done, info = self._task.step([action])
+        entry = [self._state[0], action, reward[0], next_state[0], int(done[0]), info]
+        self._total_steps += 1
+        self._state = next_state
+        return entry
+
+
+class DSRAgent(BaseAgent):
+    def __init__(self, config):
+        BaseAgent.__init__(self, config)
+        self.config = config
+        config.lock = mp.Lock()
+        
+        self.loss_q_vec = []
+        self.loss_psi_vec = []
+        self.loss_vec = []
+
+        self.replay = config.replay_fn()
+        self.actor = DSRActor(config)
+
+        self.network = config.network_fn()
+        self.network.share_memory()
+        self.target_network = config.network_fn()
+        self.target_network.load_state_dict(self.network.state_dict())
+        self.optimizer = config.optimizer_fn(self.network.parameters())
+
+        self.actor.set_network(self.network)
+
+        self.total_steps = 0
+        self.batch_indices = range_tensor(self.replay.batch_size)
+
+    def close(self):
+        close_obj(self.replay)
+        close_obj(self.actor)
+
+    def eval_step(self, state):
+        self.config.state_normalizer.set_read_only()
+        state = self.config.state_normalizer(state)
+        _, _, q = self.network(state)
+        action = to_np(q.argmax(-1))
+        self.config.state_normalizer.unset_read_only()
+        return action
+
+    def step(self):
+        config = self.config
+
+        # Store transitions in the buffer
+        transitions = self.actor.step()
+        experiences = []
+        for state, action, reward, next_state, done, info in transitions:
+            self.record_online_return(info)
+            self.total_steps += 1
+            reward = config.reward_normalizer(reward)
+            experiences.append([state, action, reward, next_state, done])
+        self.replay.feed_batch(experiences)
+
+        # Start updating network parameters after exploration_steps
+        if self.total_steps > self.config.exploration_steps:
+#             import pdb; pdb.set_trace()
+            experiences = self.replay.sample()
+            states, actions, rewards, next_states, terminals = experiences
+            states = self.config.state_normalizer(states)
+            next_states = self.config.state_normalizer(next_states)
+
+            # Computing targets
+            _, _, q_next = self.target_network(next_states)
+            _, psi_next, _ = self.network(next_states)
+            psi_next = psi_next.detach()
+            q_next = q_next.detach()
+            if self.config.double_q:
+                best_actions = torch.argmax(self.network(next_states), dim=-1)
+                q_next = q_next[self.batch_indices, best_actions]
+            else:
+                q_next = q_next.max(1)[0]
+                psi_next = psi_next.max(1)[0] # TODO: double check dims here
+
+            terminals = tensor(terminals)
+            rewards = tensor(rewards)
+            q_next = self.config.discount * q_next * (1 - terminals)
+            q_next.add_(rewards)
+            psi_next = self.config.discount * psi_next * (1 - terminals.unsqueeze(1).repeat(1, psi_next.shape[1]))
+            psi_next.add_(self.target_network(next_states)[0]) # TODO: double chec this
+
+            # Computing estimates
+            actions = tensor(actions).long()
+            _, psi, q = self.network(states)
+            q = q[self.batch_indices, actions]
+            psi = psi[self.batch_indices, actions, :]
+
+            # Estimating the loss
+            loss_q = (q_next - q).pow(2).mul(0.5).mean()
+            loss_psi = (psi_next - psi).pow(2).mul(0.5).mean()
+            loss = loss_q + config.c * loss_psi
+            
+            self.loss_vec.append(loss.item())
+            self.loss_q_vec.append(loss_q.item())
+            self.loss_psi_vec.append(loss_psi.item())
+            
+            
+            self.optimizer.zero_grad()
+            loss.backward()
+            nn.utils.clip_grad_norm_(self.network.parameters(), self.config.gradient_clip)
+
+            with config.lock:
+                self.optimizer.step()
+
+        if self.total_steps / self.config.sgd_update_frequency % \
+                self.config.target_network_update_freq == 0:
+            self.target_network.load_state_dict(self.network.state_dict())
