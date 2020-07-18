e4d7f04f458081e7baaf84137cdf83c5845f65fb
COMP202_Surya_Penmetsa <p.surya1994@gmail.com>
2020-07-18 04:00:59 -0400

    .

--- jobs.sh
+++ jobs.sh
@@ -3,13 +3,14 @@
 #SBATCH --cpus-per-task=4                     # Ask for 4 CPUs
 #SBATCH --gres=gpu:1                          # Ask for 1 GPU
 #SBATCH --mem=10G                             # Ask for 10 GB of RAM
-#SBATCH --time=24:00:00                        # The job will run for 9 hours
+#SBATCH --time=12:00:00                        # The job will run for 9 hours
 #SBATCH -o storage/slurm-%j.out  # Write the log on tmp1
 #SBATCH --exclude=eos[18-21]
 
 # 1. Load your environment
 module load anaconda
 conda activate conda_env
+wandb login
 
 
 # 3. Launch your job, tell it to save the model in $SLURM_TMPDIR
