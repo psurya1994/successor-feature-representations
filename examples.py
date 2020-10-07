3bfd7bc173a93bcc1bc70e67d0eea5b8491bf9e3
unknown <surya.penmetsa@mail.mcgill.ca>
2020-10-07 08:07:59 -0400

    .

--- examples.py
+++ examples.py
@@ -96,6 +96,51 @@ def dqn_pixel(**kwargs):
     config.async_actor = True
     run_steps(DQNAgent(config))
 
+def dqn_pixel_mini(**kwargs):
+    generate_tag(kwargs)
+    kwargs.setdefault('log_level', 0)
+    kwargs.setdefault('n_step', 1)
+    kwargs.setdefault('replay_cls', UniformReplay)
+    kwargs.setdefault('async_replay', True)
+    config = Config()
+    config.merge(kwargs)
+
+    config.task_fn = lambda: Task(config.game)
+    config.eval_env = config.task_fn()
+    config.history_length = 3
+
+    config.optimizer_fn = lambda params: torch.optim.RMSprop(
+        params, lr=0.00025, alpha=0.95, eps=0.01, centered=True)
+    config.network_fn = lambda: VanillaNet(config.action_dim, NatureConvBody(in_channels=config.history_length))
+    # config.network_fn = lambda: DuelingNet(config.action_dim, NatureConvBody(in_channels=config.history_length))
+    config.random_action_prob = LinearSchedule(1.0, 0.01, 1e6)
+    config.batch_size = 32
+    config.discount = 0.99
+    config.history_length = 3
+    config.max_steps = int(2e7)
+    replay_kwargs = dict(
+        memory_size=int(1e6),
+        batch_size=config.batch_size,
+        n_step=config.n_step,
+        discount=config.discount,
+        history_length=config.history_length,
+    )
+    config.replay_fn = lambda: ReplayWrapper(config.replay_cls, replay_kwargs, config.async_replay)
+    config.replay_eps = 0.01
+    config.replay_alpha = 0.5
+    config.replay_beta = LinearSchedule(0.4, 1.0, config.max_steps)
+
+    config.state_normalizer = ImageNormalizer()
+    config.reward_normalizer = SignNormalizer()
+    config.target_network_update_freq = 10000
+    config.exploration_steps = 50000
+    # config.exploration_steps = 100
+    config.sgd_update_frequency = 4
+    config.gradient_clip = 5
+    config.double_q = False
+    config.async_actor = True
+    run_steps(DQNAgent(config))
+
 
 # QR DQN
 def quantile_regression_dqn_feature(**kwargs):
@@ -645,7 +690,9 @@ if __name__ == '__main__':
     # game = 'BreakoutNoFrameskip-v4'
     # game = 'BoxingNoFrameskip-v4'
     game = 'PixelGridWorld'
-    dqn_pixel(game=game, n_step=1, replay_cls=UniformReplay, async_replay=False)
+    # dqn_pixel(game=game, n_step=1, replay_cls=UniformReplay, async_replay=False)
+
+    dqn_pixel_mini(game=game, n_step=1, replay_cls=UniformReplay, async_replay=False)
     # quantile_regression_dqn_pixel(game=game)
     # categorical_dqn_pixel(game=game)
     # rainbow_pixel(game=game, async_replay=False)
